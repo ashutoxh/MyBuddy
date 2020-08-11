cc4324708447b834532c04e3bf6e805f6ceb6967
Ashutosh Singh <atsingh1997@gmail.com>
2020-08-11 13:42:43 +0530

    App working now.

--- src/main/java/com/ashutoxh/buddy/buddy/BuddyApplication.java
+++ src/main/java/com/ashutoxh/buddy/buddy/BuddyApplication.java
@@ -2,19 +2,11 @@ package com.ashutoxh.buddy.buddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BuddyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(applicationClass, args);
+		SpringApplication.run(BuddyApplication.class, args);
 	}
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(applicationClass);
-	}
-
-	private static Class<BuddyApplication> applicationClass = BuddyApplication.class;
-
 }
