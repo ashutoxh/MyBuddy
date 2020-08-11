7514cd86daf0697734950b7fc5f1902b976c7bc1
Ashutosh Singh <atsingh1997@gmail.com>
2020-08-11 15:26:30 +0530

    Test

--- src/main/java/com/ashutoxh/buddy/buddy/BuddyApplication.java
+++ src/main/java/com/ashutoxh/buddy/buddy/BuddyApplication.java
@@ -1,11 +1,17 @@
 package com.ashutoxh.buddy.buddy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ashutoxh.buddy.buddy.controller.RequestController;
 
 @SpringBootApplication
 public class BuddyApplication {
 
+	@Autowired
+	RequestController restController;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BuddyApplication.class, args);
 	}
