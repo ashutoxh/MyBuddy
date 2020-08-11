f64470ac8f2810fb5cb6717bf9828ec26a18e129
Ashutosh Singh <atsingh1997@gmail.com>
2020-08-11 12:59:18 +0530

    Final update. All current modules are no working.

--- src/main/java/com/ashutoxh/buddy/buddy/BuddyApplication.java
+++ src/main/java/com/ashutoxh/buddy/buddy/BuddyApplication.java
@@ -2,12 +2,19 @@ package com.ashutoxh.buddy.buddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BuddyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuddyApplication.class, args);
+		SpringApplication.run(applicationClass, args);
 	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<BuddyApplication> applicationClass = BuddyApplication.class;
 
 }
